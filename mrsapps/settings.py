13dc9b36507fe1b11158cb5ec461de9315381f1f
benj-codes <ben-eng@outlook.com>
2023-11-24 12:46:31 +0000

    user-registeration

--- mrsapps/settings.py
+++ mrsapps/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mrsregister.apps.MrsregisterConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,8 @@ STATIC_URL = 'mrsregister/static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CSS Framework to Implement
+
+CRISPY_ALLOWED_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- users/views.py
+++ users/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -15,5 +15,5 @@ def register(request):
             messages.warning(request, 'Unable to create account!')
         return redirect('mrsregister:home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
         return render(request, 'users/register.html', {'form': form , 'title': 'Student Registration'})
